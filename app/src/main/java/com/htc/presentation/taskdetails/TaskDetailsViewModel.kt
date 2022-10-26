a9e2246eeab40eae85ad233d113e53e492ab6ac1
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2022-10-26 10:05:42 +0400

    feat: Добавлены состояния для ViewModel.

--- app/src/main/java/com/htc/presentation/taskdetails/TaskDetailsViewModel.kt
+++ app/src/main/java/com/htc/presentation/taskdetails/TaskDetailsViewModel.kt
@@ -1,5 +1,27 @@
 package com.htc.presentation.taskdetails
 
 import androidx.lifecycle.ViewModel
+import com.htc.domain.entity.Subtask
 
-class TaskDetailsViewModel: ViewModel()
+class TaskDetailsViewModel: ViewModel() {
+    data class TaskDetailsUiState(
+        /**
+         * Описание задачи.
+         */
+        val description: String = "",
+        /**
+         * Завершённость задачи.
+         */
+        val state: Boolean = false,
+        /**
+         * Список подзадач.
+         */
+        val subtasks: List<Subtask> = emptyList(),
+        /**
+         * Признак того, что приложение находится в процессе загрузки данных.
+         */
+        val showLoading: Boolean = false,
+    )
+
+    private val uiState = TaskDetailsUiState()
+}
--- app/src/main/java/com/htc/presentation/tasklist/TaskListViewModel.kt
+++ app/src/main/java/com/htc/presentation/tasklist/TaskListViewModel.kt
@@ -1,5 +1,19 @@
 package com.htc.presentation.tasklist
 
 import androidx.lifecycle.ViewModel
+import com.htc.domain.entity.Task
 
-class TaskListViewModel : ViewModel()
+class TaskListViewModel : ViewModel() {
+    data class TaskListUiState(
+        /**
+         * Список задач.
+         */
+        val tasks: List<Task> = emptyList(),
+        /**
+         * Признак того, что приложение находится в процессе загрузки данных.
+         */
+        val showLoading: Boolean = false,
+    )
+
+    private val uiState = TaskListUiState()
+}
