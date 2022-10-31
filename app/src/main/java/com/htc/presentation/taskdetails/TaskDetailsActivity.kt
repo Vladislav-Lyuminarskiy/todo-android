a9031c9e4f551c450f2d5bcc7c45313b020c73bd
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2022-10-31 13:46:41 +0400

    feat: Добавлен метод для запуска Activity.

--- app/src/main/java/com/htc/presentation/taskdetails/TaskDetailsActivity.kt
+++ app/src/main/java/com/htc/presentation/taskdetails/TaskDetailsActivity.kt
@@ -1,5 +1,7 @@
 package com.htc.presentation.taskdetails
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.htc.R
@@ -8,5 +10,12 @@ class TaskDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_details)
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, TaskDetailsActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
