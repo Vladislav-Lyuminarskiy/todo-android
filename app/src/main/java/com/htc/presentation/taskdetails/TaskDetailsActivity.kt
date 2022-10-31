4ba164792a6247badef945483766641f2c0bacbf
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2022-10-31 16:15:48 +0400

    feat: Добавлено открытие экрана задачи по нажатию на задачу в списке.

--- app/src/main/java/com/htc/presentation/taskdetails/TaskDetailsActivity.kt
+++ app/src/main/java/com/htc/presentation/taskdetails/TaskDetailsActivity.kt
@@ -3,18 +3,39 @@ package com.htc.presentation.taskdetails
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.htc.R
+import com.htc.presentation.tasklist.TaskListActivity
 
 class TaskDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_details)
+
+        val taskId = intent.getIntExtra("taskId", -1)
+        val task = TaskListActivity.tasks.find { it.id == taskId }
+
+        if (task == null || taskId == -1) {
+            Toast.makeText(
+                this,
+                "Задача с указанным идентификатором не существует",
+                Toast.LENGTH_LONG
+            ).show()
+
+            finish()
+        }
+
+        findViewById<TextView>(R.id.toolbar_title).text = task?.description
     }
 
     companion object {
-        fun start(context: Context) {
-            val intent = Intent(context, TaskDetailsActivity::class.java)
+        fun start(context: Context, taskId: Int) {
+            val intent = Intent(context, TaskDetailsActivity::class.java).apply {
+                putExtra("taskId", taskId)
+            }
+
             context.startActivity(intent)
         }
     }
--- app/src/main/java/com/htc/presentation/tasklist/TaskListActivity.kt
+++ app/src/main/java/com/htc/presentation/tasklist/TaskListActivity.kt
@@ -9,48 +9,59 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.htc.R
 import com.htc.domain.entity.Subtask
 import com.htc.domain.entity.Task
+import com.htc.presentation.taskdetails.TaskDetailsActivity
 import io.reactivex.rxjava3.core.Flowable
 import kotlin.random.Random
 
 class TaskListActivity : AppCompatActivity() {
-    val tasks: List<Task> by lazy {
-        // TODO: Заменить фейковые данные на реальные.
-        val tasks: MutableList<Task> = mutableListOf()
-        for (i in 1..20) {
-            val subtasks: MutableList<Subtask> = mutableListOf()
-            val subtaskCount = Random.nextInt(4)
-            for (j in 1..subtaskCount) {
-                subtasks.add(Subtask(
-                    id = i + j,
-                    description = "Subtask $i",
-                    status = Random.nextBoolean()
-                ))
-            }
-
-            tasks.add(Task(
-                id = i,
-                description = "Task $i",
-                status = Random.nextBoolean(),
-                Flowable.empty()
-            ))
-        }
-
-        tasks
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = TaskListAdapter(tasks) {
-            Toast.makeText(this, it.description, Toast.LENGTH_SHORT).show()
+        findViewById<RecyclerView>(R.id.recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@TaskListActivity)
+            adapter = TaskListAdapter(tasks) {
+                Toast.makeText(
+                    this@TaskListActivity,
+                    it.description,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                TaskDetailsActivity.start(this@TaskListActivity, it.id)
+            }
         }
+
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             Toast.makeText(this, "FAB was clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        val tasks: List<Task> by lazy {
+            // TODO: Заменить фейковые данные на реальные.
+            val tasks: MutableList<Task> = mutableListOf()
+            for (i in 1..20) {
+                val subtasks: MutableList<Subtask> = mutableListOf()
+                val subtaskCount = Random.nextInt(4)
+                for (j in 1..subtaskCount) {
+                    subtasks.add(Subtask(
+                        id = i + j,
+                        description = "Subtask $i",
+                        status = Random.nextBoolean()
+                    ))
+                }
+
+                tasks.add(Task(
+                    id = i,
+                    description = "Task $i",
+                    status = Random.nextBoolean(),
+                    Flowable.empty()
+                ))
+            }
+
+            tasks
         }
     }
 }
